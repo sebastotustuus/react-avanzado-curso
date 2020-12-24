0ad72d2a4a54fd10600bd0b25b551db75ed563bf
Sebastian Vallejo <pro-svallejo@intergrupo.com>
2020-12-24 15:08:18 -0500

    feat: implementacion configuracion inicial

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(<h1>Hola Mundo Platzi!</h1>, document.getElementById("App"));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "index.html",
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
+};
